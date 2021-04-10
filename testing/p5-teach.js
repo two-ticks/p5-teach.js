5a3531ee14dcc3427844ac0230678a953bd6e26e
TwoTicks <adityasiddheshwar.adisid@gmail.com>
2021-04-11 00:21:01 +0530

    testing multiple functions

--- testing/p5-teach.js
+++ testing/p5-teach.js
@@ -1,3 +1,8 @@
-module.exports = function(){
-    return 'hello';
+module.exports = {
+    sayHello: function(){
+        return 'hello';
+    },
+    addNumbers: function(val1,val2){
+        return val1+val2;
+    }
 }
