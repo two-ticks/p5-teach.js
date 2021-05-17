e980ea5298ae9667ec275da0494b24279d79c2d2
two-ticks <adityasiddheshwar.adisid@gmail.com>
2021-05-17 15:01:25 +0530

    :page_facing_up: index.html

--- p5-teach/sketch.js
+++ p5-teach/sketch.js
@@ -7,8 +7,8 @@ function draw() {
   //typeWriter("Hello! p5.js", 0, width / 100, height / 1.75, 250); //sentence, n, x, y, delay
   //fadeIn("p5");
  
-  drawGrid();
-  writing(" writing the text", 0,0, width / 100, height / 1.75, 50);
+  //drawGrid();
+  writing("p5-teach.js", 0,0, width / 7, height / 1.75, 50);
   
   noLoop();
 }
